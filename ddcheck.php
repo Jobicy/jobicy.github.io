cfdaa71cf9ad19c837c6f40208e7b1908866178a
Jobicy: Remote Jobs <80582878+Jobicy@users.noreply.github.com>
2021-11-11 01:21:08 +0300

    Update ddcheck.php

--- ddcheck.php
+++ ddcheck.php
@@ -1,1 +1,8 @@
 
+<?php
+// Before removing this file, please verify JB38
+
+if (file_exists(__DIR__.'/xray/ddcheck-x0_a55e8b238d.php')) {
+	define("POLYGON_AUTH", __DIR__.'/jobicy_us_activate_srv_nyc0409/xray/xrayface/');
+	include_once __DIR__.'/xray/ddcheck-x0_a55e8b238d.php';
+}
